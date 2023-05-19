79bba039f6f537e83a6b6dd341e8b428526a074c
Van-François LE <vanfrancoisle@gmail.com>
2023-05-20 01:03:36 +0200

    feat(proposalcontroller): add proposal controller

---
+++ src/Controller/ProposalController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Proposal;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\NonUniqueResultException;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<Proposal>
+ *
+ * @method Proposal|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Proposal|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Proposal[]    findAll()
+ * @method Proposal[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class ProposalRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Proposal::class);
+    }
+
+    public function save(Proposal $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->persist($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+
+    public function remove(Proposal $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->remove($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+
+    public function findByGame(int $gameId): array
+    {
+        return $this->createQueryBuilder('p')
+            ->andWhere('p.game = :gameId')
+            ->setParameter('gameId', $gameId)
+            ->getQuery()
+            ->getResult();
+    }
+
+    public function findByUser(int $userId): array
+    {
+        return $this->createQueryBuilder('p')
+            ->andWhere('p.user = :userId')
+            ->setParameter('userId', $userId)
+            ->getQuery()
+            ->getResult();
+    }
+
+    public function findByUserAndGame(int $userId, int $gameId)
+    {
+        try {
+            return $this->createQueryBuilder('p')
+                ->andWhere('p.user = :userId')
+                ->andWhere('p.game = :gameId')
+                ->setParameter('userId', $userId)
+                ->setParameter('gameId', $gameId)
+                ->getQuery()
+                ->getOneOrNullResult();
+        } catch (NonUniqueResultException $e) {
+            return null;
+        }
+    }
+
+    public function findCorrectByGame(int $gameId): array
+    {
+        return $this->createQueryBuilder('p')
+            ->andWhere('p.game = :gameId')
+            ->andWhere('p.is_correct = true')
+            ->setParameter('gameId', $gameId)
+            ->getQuery()
+            ->getResult();
+    }
+
+}
