d28d934acd706ab9f2508ea5a95db89fc98f30f5
Van-François LE <vanfrancoisle@gmail.com>
2023-05-25 15:00:00 +0200

    feat(accountcontroller): add is user logged in

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -2,17 +2,19 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Controller\BaseController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class AccountController extends AbstractController
+class AccountController extends BaseController
 {
     #[Route('/account', name: 'app_account')]
     public function index(): Response
     {
+        $isLogged = $this->isLoggedIn();
         return $this->render('account/index.html.twig', [
             'controller_name' => 'AccountController',
+            'is_logged_in' => $isLogged,
         ]);
     }
 }
