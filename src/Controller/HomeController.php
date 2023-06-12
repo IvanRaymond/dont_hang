2668ab288a2369e085133ef454a6384b294dba36
Van-François LE <vanfrancoisle@gmail.com>
2023-06-12 13:37:23 +0200

    feat(HomeController): only game master can create a game

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -15,11 +15,18 @@ class HomeController extends BaseController
     public function index(EntityManagerInterface $entityManager): Response
     {
         $isLogged = $this->isLoggedIn();
+        if ($isLogged) {
+            $isAdmin = $this->getUser()->isMaster();
+        } else {
+            $isAdmin = false;
+        }
+        
         $avatar = $this->getUser() ? $this->getUser()->getPicture() : '';
 
         $rooms = $entityManager->getRepository(Room::class)->findAll();
         return $this->render('home/index.html.twig', [
             'is_logged_in' => $isLogged,
+            'is_admin' => $isAdmin,
             'avatar' => $avatar,
             'controller_name' => 'HomeController',
             'rooms' => $rooms,
