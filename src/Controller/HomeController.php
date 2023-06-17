baa22a83b8ff4a0053af1860dd4df5dd3079de5d
Van-François LE <vanfrancoisle@gmail.com>
2023-06-17 10:17:36 +0200

    feat: add form to create party

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,14 +5,16 @@ namespace App\Controller;
 use App\Controller\BaseController;
 use App\Entity\Game;
 use App\Entity\Room;
+use App\Form\CreateGameType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends BaseController
 {
     #[Route('/', name: 'app_home')]
-    public function index(EntityManagerInterface $entityManager): Response
+    public function index(Request $request, EntityManagerInterface $entityManager): Response
     {
         $isLogged = $this->isLoggedIn();
         if ($isLogged) {
@@ -20,7 +22,41 @@ class HomeController extends BaseController
         } else {
             $isAdmin = false;
         }
-        
+
+        $game = new Game();
+        $form = $this->createForm(CreateGameType::class, $game);
+        $form->handleRequest($request);
+
+        // if ($form->isSubmitted() && $form->isValid()) {
+            // $game->setRoom($room);
+            // $game->setDuration($duration);
+            // $game->setWord($word);
+            // $game->setWordStatus($init);
+            // $picture = $photoForm->get('picture')->getData();
+
+            // if ($picture->getClientOriginalExtension() != "jpeg") {
+            //     // display a snackbar error
+            //     $this->addFlash('success', 'Mauvaise extension du fichier. (jpeg)');
+            //     return $this->redirectToRoute('app_account_edit');
+            // }
+
+            // // move the file to the right folder
+            // $pictureUrl = uniqid().'.jpeg';
+            // $picture->move(
+            //     "assets/users",
+            //     $pictureUrl
+            // );
+
+            // $this->getUser()->setPicture($pictureUrl);
+
+            // $entityManager->persist($user);
+            // $entityManager->flush();
+
+            // display a snackbar success
+            // $this->addFlash('success', 'Photo de profil modifiée.');
+            // return $this->redirectToRoute('app_account');
+        // }
+
         $avatar = $this->getUser() ? $this->getUser()->getPicture() : '';
 
         $rooms = $entityManager->getRepository(Room::class)->findAll();
@@ -30,6 +66,7 @@ class HomeController extends BaseController
             'avatar' => $avatar,
             'controller_name' => 'HomeController',
             'rooms' => $rooms,
+            'createGameForm' => $form->createView(),
         ]);
     }
 }
