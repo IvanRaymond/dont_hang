4b611e4566109ccd15034d5278b5a7732cef577f
ivanraymond <ivan.raymond@protonmail.com>
2023-06-17 12:25:44 +0200

    fix(front): Creating game JS logic

---
+++ public/js/game.js
@@ -0,0 +1,15 @@
+console.log('game.js loaded');
+
+// Global variables
+// The game data
+
+// Check if a game is ongoing by doing a GET request to /api/game
+
+// If so, load the game data
+// If not, show waiting screen
+// If game is over, show game over screen
+
+// Show modal to ask for letter or word proposal
+// Update animation based on the response
+// Repeat until game is over
+
